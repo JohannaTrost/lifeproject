3aa02388cbca17146820a978df29b70560c2524d
TommyClausner <tommy.clausner@gmail.com>
2019-12-09 19:35:53 +0100

    update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from src.simulation import simulate_pop
 from src.evolution import make_random_pop
 
-motion_pattern_duration = 2  # in seconds
+motion_pattern_duration = 1  # in seconds
 fps = 240  # frames per second
 move_steps = motion_pattern_duration * fps
 
-genomes = make_random_pop(num_inds=2, move_steps=move_steps)
+genomes = make_random_pop(num_inds=1, move_steps=move_steps)
 
-new_genomes = simulate_pop(genomes, fps=fps)
+new_genomes = simulate_pop(genomes, fps=fps, move_steps=move_steps)
