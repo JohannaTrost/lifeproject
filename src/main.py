b0dc7ebdece372db64b0543799ac4ee47b1a6d66
TommyClausner <tommy.clausner@gmail.com>
2019-12-10 17:00:37 +0100

    add save / load

--- src/main.py
+++ src/main.py
@@ -1,33 +1,43 @@
 from src.simulation import simulate_pop, reset_simulation
-from src.evolution import make_random_gene_pool, selection, crossing
-from src.stats import avg_dist, pop_stats
+import src.evolution as evo
+import src.stats as st
 
 motion_pattern_duration = 2  # in seconds
 fps = 240  # frames per second
 move_steps = int(motion_pattern_duration * fps)
-show_best = 1
+
+show_best = 0
 duration_per_simulation_in_sec = 10
-generations = 10
+generations = 3
 individuals = 20
 stats = []
+best = 0
 
-gene_pool = make_random_gene_pool(num_inds=individuals, move_steps=move_steps)
+gene_pool = evo.make_random_gene_pool(num_inds=individuals, move_steps=move_steps)
 
 for generation in range(generations):
     pop, sim_id = simulate_pop(gene_pool, fps=fps, duration_in_sec=duration_per_simulation_in_sec, direct=True)
 
-    print('generation {} | avg distance {}'.format(generation, avg_dist(pop)))
+    avg_dist = st.avg_dist(pop)
 
-    # collect data on population
-    stats = pop_stats(stats, pop, generation)
+    print('generation {} | avg distance {}'.format(generation, avg_dist))
 
     # evolute
-    gene_pool = crossing(selection(pop), gene_pool)
+    selected = evo.selection(pop)
+    best = selected[0][0]
+    gene_pool = evo.crossing(selected, gene_pool)
+
+    # collect data on population
+    stats.append([generations, avg_dist, best])
 
     # reset simulation
     reset_simulation(sim_id)
 
+st.save_stats(stats)
+evo.save_gene_pool(gene_pool)
+
 if show_best:
     # show best parent
-    simulate_pop([gene_pool[selection(pop)[1][0]]],
-                 fps=fps, duration_in_sec=-1, direct=False)
+    best = st.load_stats()[-1, -1]
+    gene_pool = evo.load_gene_pool()
+    simulate_pop([gene_pool[int(best)]], fps=fps, duration_in_sec=-1, direct=False)
--- src/stats.py
+++ src/stats.py
@@ -6,13 +6,9 @@ def avg_dist(pop):
     return np.mean([get_dist(ind) for ind in pop])
 
 
-def pop_stats(stats, pop, generation):
-    """
-    All data that is supposed to be collected could go in here.
-    :param stats:
-    :param pop:
-    :param generation:
-    :return:
-    """
-    stats.append([generation, avg_dist(pop)])
-    return stats
+def save_stats(stats, filename='src/results/latest_results.csv'):
+    np.savetxt(filename, stats, delimiter=',')
+
+
+def load_stats(filename='src/results/latest_results.csv'):
+    return np.loadtxt(filename, delimiter=',')
