8ff1e4f8ed792811d362c8cf2334199af3a4478c
TommyClausner <tommy.clausner@gmail.com>
2020-01-02 19:22:13 +0100

    update

--- simulate_evolution_qsub.sh
+++ simulate_evolution_qsub.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-tmpFile=`mktemp -d -p "$DIR"`
-sed "2i DIR=$DIR" $DIR/simulate_evolution.sh > tmpFile.sh
-qsub -q batch -l walltime=23:59:00,mem=16gb -F "$@" tmpFile.sh &
+tmpFile=`mktemp -d -p "$DIR"`.sh
+sed "2i DIR=$DIR" $DIR/simulate_evolution.sh > $tmpFile
+qsub -q batch -l walltime=23:59:00,mem=16gb -F "$@" $tmpFile &
 sleep 2s
 rm $tmpFile
